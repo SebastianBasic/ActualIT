90c0eb69f24849c63dadbeaab5be2c162b2a6bab
Sebastian <s.basic12321@gmail.com>
2021-12-28 17:29:22 +0100

    Kreiran je osnovni kostur komponenti za frontend aplikaciju

--- ActualFrontEnd/angular10/src/app/app.module.ts
+++ ActualFrontEnd/angular10/src/app/app.module.ts
@@ -3,16 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AddressbookComponent } from './addressbook/addressbook.component';
+import { UpsertComponent } from './addressbook/upsert/upsert.component';
+import { SharedService } from './shared.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddressbookComponent,
+    UpsertComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [SharedService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
