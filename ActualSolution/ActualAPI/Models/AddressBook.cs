e66e0d318659e90f1267e70cb11eca197230e83c
Sebastian <s.basic12321@gmail.com>
2021-12-28 15:30:10 +0100

    postavljena je bazižna API aplikacija sa odgovarajućim modelom i migracijama

---
+++ ActualSolution/ActualAPI/Models/AddressBook.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ActualAPI.Models
+{
+    public class AddressBook
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string FirstName { get; set; }
+        [Required]
+        public string LastName { get; set; }
+        [Required]
+        public string Adress { get; set; }
+        [Required]
+        public string TelNumber { get; set; }
+    }
+}
